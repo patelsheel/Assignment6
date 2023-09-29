20be3a48c9b0af56c3adad89c7983f78d77bb3b8
patelsheel <77370692+patelsheel@users.noreply.github.com>
2023-09-28 23:49:08 -0400

    Initial commit

---
+++ BlazorPizzaStore/wwwroot/lib/bootstrap/css/bootstrap.bundle.js
@@ -0,0 +1,20 @@
+<html>
+<head><title>404 Not Found</title></head>
+<body>
+<h1>404 Not Found</h1>
+<ul>
+<li>Code: NoSuchKey</li>
+<li>Message: The specified key does not exist.</li>
+<li>Key: public/bootstrap/5.3.0/js/bootstrap.bundle.js</li>
+<li>RequestId: 3EBD2ME4R1W0SFNZ</li>
+<li>HostId: vpgSiOvMaE9s+2qcNN2yeqK0DsrFKi2J6/cLF4c4PLUIsmij0v3+0btmtvd1GIvqOXiCN1+XBBI=</li>
+</ul>
+<h3>An Error Occurred While Attempting to Retrieve a Custom Error Document</h3>
+<ul>
+<li>Code: NoSuchKey</li>
+<li>Message: The specified key does not exist.</li>
+<li>Key: index.html</li>
+</ul>
+<hr/>
+</body>
+</html>
