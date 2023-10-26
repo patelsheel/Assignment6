dd67d9ef79b676329f05693e449bf6d17369ed07
patelsheel <patel7sa@mail.uc.edu>
2023-10-26 01:28:08 -0400

    Configured Products and POS

---
+++ PizzaStoreModels/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace PizzaStoreModels
+{
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        [MaxLength(50)]
+        [DisplayName("Product")]
+        public string? ProductName { get; set; }
+        [DisplayName("Price")]
+        public float ProductPrice { get; set; }
+    }
+}
+
